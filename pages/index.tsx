7046ff6245b9b2555b31752120982bda8724039a
Syed Adeeb <abidi9897401278@gmail.com>
2022-09-01 15:53:35 +0530

    Bug fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,19 +15,21 @@ const Home: NextPage = () => {
 	const addTaskToTable = (val: any) => {
 		val.preventDefault();
 		setUserInput([task, ...userInput]);
-    setTask('')
+		setTask("");
 	};
 
-  const deleteTask = (task: any) => {
-    const updatedArray = userInput.filter(userItem => userInput.indexOf(userItem) != userInput.indexOf(task))
-    setUserInput(updatedArray)
-  }
+	const deleteTask = (task: any) => {
+		const updatedArray = userInput.filter(
+			(userItem) => userInput.indexOf(userItem) != userInput.indexOf(task)
+		);
+		setUserInput(updatedArray);
+	};
 
 	return (
-		<div className="">
+		<section className="text-gray-600 body-font relative">
 			<Navbar />
-			<div>
-				<section className="text-gray-600 body-font relative">
+			<div className="">
+				<div>
 					<div className="container px-5 py-24 mx-auto">
 						<div className="flex flex-col text-center w-full mb-12">
 							<h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
@@ -65,73 +67,66 @@ const Home: NextPage = () => {
 							</div>
 						</div>
 					</div>
-				</section>
-			</div>
-			<div>
-				<div className="flex flex-col md:mx-40">
-					<div className=" sm:-mx-6 lg:-mx-8">
-						<div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-							<div className="overflow-hidden">
-								<table className="min-w-full">
-									<thead className="border-b">
-										<tr>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												S.No
-											</th>
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												Date
-											</th>
+				</div>
+				<div>
+					<div className="flex flex-col md:mx-40">
+						<div className=" sm:-mx-6 lg:-mx-8 overflow-auto">
+							<div className="py-2 inline-block min-w-full sm:px-6 lg:px-20">
+								<div className="overflow-hidden">
+									<table className="min-w-full truncate">
+										<thead className="border-b">
+											<tr>
+												<th
+													scope="col"
+													className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+												>
+													S.No
+												</th>
 
-											<th
-												scope="col"
-												className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-											>
-												Task
-											</th>
-										</tr>
-									</thead>
-									<tbody>
-										{userInput.length > 0
-											? userInput.map((task, i) => (
-													<tr className="border-b" key={i}>
-														<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-															{i + 1}
-														</td>
-														<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-															Mark
-														</td>
-														<td className="flex text-sm text-gray-900 font-light px-6 py-4 items-center whitespace-nowrap">
-															{task}
-															<div className="ml-auto">
-																<button
-																	onClick={(e) => {
-																		e.preventDefault();
-																		deleteTask(task);
-																	}}
-																	className="bg-red-400 px-4 py-1 rounded-md text-white font-medium"
-																>
-																	Delete
-																</button>
-															</div>
-														</td>
-													</tr>
-											  ))
-											: ""}
-									</tbody>
-								</table>
-								{userInput.length === 0 && <p className="text-center text-xl mt-10 "> No task added</p>}
+												<th
+													scope="col"
+													className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+												>
+													Task
+												</th>
+											</tr>
+										</thead>
+										<tbody>
+											{userInput.length > 0
+												? userInput.map((task, i) => (
+														<tr className="border-b " key={i}>
+															<td className="px-6 py-4 sm:text-lg font-medium  text-gray-900">
+																{i + 1}
+															</td>
+															<td className="flex md:text-lg text-gray-900 font-semibold px-6 py-4 items-center">
+																<h1 className="truncate">{task}</h1>
+																<div className="ml-auto">
+																	<button
+																		onClick={(e) => {
+																			e.preventDefault();
+																			deleteTask(task);
+																		}}
+																		className="border-red px-4 py-2 ml-2 rounded-full text-red-600 font-medium"
+																	>
+																		Remove
+																	</button>
+																</div>
+															</td>
+														</tr>
+												  ))
+												: ""}
+										</tbody>
+									</table>
+									{userInput.length === 0 && (
+										<p className="text-center text-xl mt-10 "> No task added</p>
+									)}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
