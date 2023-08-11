deb60252e44a6f9cbec6e3970f04d53ab66f471f
Syed Adeeb <prokximus@gmail.com>
2023-08-11 16:41:19 +0530

    update 1.1

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
 
 	const [task, setTask] = useState("");
 	const [userInput, setUserInput] = useState([]);
-
+	
 	const AddTask = (val) => {
 		val.preventDefault();
 
@@ -37,7 +37,7 @@ const Home = () => {
 					<div className="container px-5 py-24 mx-auto">
 						<div className="flex flex-col text-center w-full mb-12">
 							<h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-								Add a task
+								Add a task.
 							</h1>
 						</div>
 						<div className="lg:w-1/2 md:w-2/3 mx-auto">
